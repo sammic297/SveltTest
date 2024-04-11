ead094ac478ec71273fc27daa32f8a666abf6a13
Odey, Samuel Michael <sammic297@gmail.com>
2024-04-11 13:27:24 +0100

    push

---
+++ src/routes/Contact-Us/+page.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+    <title>Contact Us</title>
+    <meta name="description" content="About this app">
+</svelte:head>
+
+<div class="text-column">
+    <center>
+        <h1>Mi Nombre es Odey</h1> 
+        <p><em>Email:</em> sammic297@gmail.com</p>
+        <p><em>Phone:</em> (+234) 8130967626</p>
+    </center>
+</div>
